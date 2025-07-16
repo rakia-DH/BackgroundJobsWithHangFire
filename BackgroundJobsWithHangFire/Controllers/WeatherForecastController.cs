f71e805a7965bc9a78a2fb663bd3df31161c8fc2
rakia <rakia.abdelaziz@digitalhub.com.eg>
2025-07-16 17:07:28 +0300

    scadule task

--- BackgroundJobsWithHangFire/Controllers/WeatherForecastController.cs
+++ BackgroundJobsWithHangFire/Controllers/WeatherForecastController.cs
@@ -23,7 +23,9 @@ namespace BackgroundJobsWithHangFire.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             //immediatly
-            BackgroundJob.Enqueue(()=>SendMessages( "raqya18200@gmail.com"));
+            // BackgroundJob.Enqueue(()=>SendMessages( "raqya18200@gmail.com"));
+            Console.WriteLine(DateTime.Now);
+            BackgroundJob.Schedule(() => SendMessages("raqya18200@gmail.com"),TimeSpan.FromMinutes(1));
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -36,8 +38,11 @@ namespace BackgroundJobsWithHangFire.Controllers
         [ApiExplorerSettings(IgnoreApi =true)]
         public void SendMessages(string Email)
         {
-            Console.WriteLine(value: "Email sent");
+            Console.WriteLine($"Email sent at {DateTime.Now}");
         }
+
+
+
 
     }
 }
